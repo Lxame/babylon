61caeb13b87f68aaae2e0925a0e002d87af8c2b0
Matvey Starkov <m.a.starkov@stud.mospolytech.ru>
2025-03-13 13:31:41 +0300

    added movement by keys pressed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { addCoordInScene } from "./utils.js";
 const canvas = document.querySelector("#game-canvas");
 const engine = new BABYLON.Engine(canvas, true, { stencil: true }, true);
 
+let cubeSkelet;
+
 const cubeSkeletonScene = function (size) {
 	var scene = new BABYLON.Scene(engine);
 
@@ -32,7 +34,7 @@ const cubeSkeletonScene = function (size) {
 	}
 	console.log(pointsArr);
 
-	const cubeSkelet = BABYLON.Mesh.CreateLines("cubeSkelet", points, scene);
+	cubeSkelet = BABYLON.Mesh.CreateLines("cubeSkelet", points, scene);
 	cubeSkelet.color = new BABYLON.Color3(0, 0, 0);
 
     return scene;
@@ -46,14 +48,37 @@ engine.runRenderLoop(() => {
 
 cubeScene.onKeyboardObservable.add((kbInfo) => {
     switch (kbInfo.type) {
-      case BABYLON.KeyboardEventTypes.KEYDOWN:
-        console.log("KEY DOWN: ", kbInfo.event.key);
-        if (kbInfo.event.key === "w") {
-            console.log("WW");
-        }
-        break;
-      case BABYLON.KeyboardEventTypes.KEYUP:
-        console.log("KEY UP: ", kbInfo.event.code);
-        break;
+        case BABYLON.KeyboardEventTypes.KEYDOWN:
+            console.log("KEY DOWN: ", kbInfo.event.key);
+            switch (kbInfo.event.key) {
+                case "a":
+                case "A":
+                    cubeSkelet.position.x -= 0.1;
+                    break
+                case "d":
+                case "D":
+                    cubeSkelet.position.x += 0.1;
+                break
+                case "w":
+                case "W":
+                    cubeSkelet.position.y += 0.1;
+                    break
+                case "s":
+                case "S":
+                    cubeSkelet.position.y -= 0.1;
+                    break
+                case "q":
+                case "Q":
+                    cubeSkelet.position.z -= 0.1;
+                    break
+                case "e":
+                case "E":
+                    cubeSkelet.position.z += 0.1;
+                    break                
+            }
+            break;
+        case BABYLON.KeyboardEventTypes.KEYUP:
+            console.log("KEY UP: ", kbInfo.event.code);
+            break;
     }
-  });
+});
